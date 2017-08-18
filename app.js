c7e3f5cf8941d7358caf3ba25fe584cbe273fda2
Shannon <reidy.pf@gmail.com>
2017-08-18 15:27:41 -0500

    validation not working

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const parseurl = require('parseurl');
 const session = require('express-session');
+const bodyParser = require('body-parser')
+const expressValidator = require('express-validator');
 
 let app = express();
 app.engine('mustache', mustacheExpress());
@@ -14,8 +16,44 @@ app.use(session({
   saveUninitialized: true
 }))
 
+//an object that has our array of letters to guess
+let lettersGuessed = {
+  letters:[]
+}
+
+//so we can read in a form
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(expressValidator());
+
+//find out why we would use this
+app.use(express.static('public'));
+
+//pull up our site first
 app.get('/', function(req, res, next){
   res.render('wordgame')
+})
+//post once we get our input
+app.post('/', function(req, res, next){
+  let guessLetter = req.body.guessLetter;
+
+//check for valid input
+req.checkBody('guessLetter', "You must type something").notEmpty();
+let errors = req.validationErrors();
+
+console.log(errors);
+//check if we've guessed the letter before
+ let alreadyGuessed = false;
+  for (let i = 0; i < lettersGuessed.letters.length; i++) {
+    if(lettersGuessed.letters[i] === guessLetter){
+      alreadyGuessed = true;
+    }
+  }
+
+  //add our letters to an array of letters guessed
+  if (!alreadyGuessed) {
+    lettersGuessed.letters.push(guessLetter);
+  }
+  res.render('wordgame', {lettersGuessed:lettersGuessed.letters, errs:errors});
 })
 
 app.listen(3000, function(){
